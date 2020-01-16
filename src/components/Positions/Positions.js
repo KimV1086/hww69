95fc4e510a0b3e5f0e47e5abcb03139ace0bf31f
Vitalik Kim <kimv1086@gmail.com>
2020-01-16 13:29:11 +0600

    delete reacstrap

--- src/components/Positions/Positions.js
+++ src/components/Positions/Positions.js
@@ -1,18 +1,16 @@
 import React from "react";
-
 import "./Positions.css";
 
 const Positions = props => {
+
     return (
         <div className="Position">
-            <Card>
-                <CardImg top width="100%" src={props.image} alt={props.name} />
-                <CardBody>
-                    <CardTitle>{props.name}</CardTitle>
-                    <CardSubtitle><b>Price: </b>{props.price} kgs</CardSubtitle>
-                    <Button onClick={props.onClick}>Add to cart</Button>
-                </CardBody>
-            </Card>
+            <div>
+                <img src={props.image} alt=""/>
+                <p>{props.name}</p>
+                <p><b>Price: </b>{props.price} kgs</p>
+                <button onClick={props.onClick}>Add to cart</button>
+            </div>
         </div>
     )
 };
